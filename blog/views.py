adf7228e5c7385b6e18fad5e88918622e8a6c5cf
Wahid Ali <aliwahid1716@gmail.com>
2022-06-30 12:42:16 +0530

    added some new files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def blogHome(request):
+    return render(request, 'blog/blogHome.html')
+    # return HttpResponse('This is  blog home. We will keep all blog posts here')
+
+
+def blogPost(request, slug):
+    return render(request, 'blog/blogPost.html')
+    # return HttpResponse(f'This is blogPost : {slug}')
