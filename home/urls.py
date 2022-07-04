a44c7d272d7e7183c1cbecb7738557c6f1c67323
Wahid Ali <aliwahid1716@gmail.com>
2022-07-04 11:37:27 +0530

    added signup and login form

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('contact', views.contact, name="contact"),
     path('about', views.about, name="about"),
     path('search', views.search, name="search"),
+    path('signup', views.handleSignUp, name="handleSignUp"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+import imp
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 from home.models import Contact
 from blog.models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -16,7 +18,8 @@ def contact(request):
         email = request.POST['email']
         phone = request.POST['phone']
         content = request.POST['content']
-        if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(content) < 4:
+        if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(
+                content) < 4:
             messages.error(request, "Please fill the form correctly")
         else:
             contact = Contact(name=name,
@@ -32,6 +35,7 @@ def contact(request):
 def about(request):
     return render(request, 'home/about.html')
 
+
 def search(request):
     query = request.GET['query']
     if len(query) > 78:
@@ -42,7 +46,31 @@ def search(request):
         allPostsContent = Post.objects.filter(content__icontains=query)
         allPosts = allPostsTitle.union(allPostsContent, allPostsAuthor)
     if allPosts.count() == 0:
-        messages.warning(
-            request, "No search results found. Please refine your query.")
+        messages.warning(request,
+                         "No search results found. Please refine your query.")
     params = {'allPosts': allPosts, 'query': query}
     return render(request, 'home/search.html', params)
+
+
+def handleSignUp(request):
+    if request.method == "POST":
+        # Get the post parameters
+        username = request.POST['username']
+        email = request.POST['email']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        # check for errorneous input
+
+        # Create the user
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, " Your iCoder has been successfully created")
+        return redirect('home')
+
+    else:
+        return HttpResponse("404 - Not found")
